c9b24cdf1a91d0956b8fecb0b45f45d429bb76e0
Colton Phass <coltonsphass@gmail.com>
2025-07-22 18:24:23 -0400

    testing

---
+++ js/welcome.js
@@ -0,0 +1,8 @@
+function blackJack() {
+    window.location.href = "../html/blackjack.html";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const blackjackButton = document.getElementById("blackjack");
+    blackjackButton.addEventListener("click", blackJack);
+});
